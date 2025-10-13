9694ea0448086fe6923d7f3daf6129ab86b31518
udinicl-sku-cpp-iet <udinicl.dev@gmail.com>
2025-10-13 22:52:37 +0200

    finish challenge 9

---
+++ Set2/Challenge9/Challenge_9.py
@@ -0,0 +1,9 @@
+def pkcs7_pad(data: bytes, block_size: int) -> bytes:
+    padding_len = block_size - (len(data) % block_size)
+    padding = bytes([padding_len] * padding_len)
+    return data + padding
+
+
+text = b"YELLOW SUBMARINE"
+
+print(pkcs7_pad(text,20))
